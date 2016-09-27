ec0fe0794db181d43b8eaa73791e4e835e93ca13
VietAnhh <viet.le175@gmail.com>
2016-09-26 23:59:01 -0400

    first release

---
+++ index.ts
@@ -0,0 +1,290 @@
+'use strict';
+
+function test(){
+
+}
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: 'PlainText',
+            text: output,
+        },
+        card: {
+            type: 'Simple',
+            title: `SessionSpeechlet - ${title}`,
+            content: `SessionSpeechlet - ${output}`,
+        },
+        reprompt: {
+            outputSpeech: {
+                type: 'PlainText',
+                text: repromptText,
+            },
+        },
+        shouldEndSession,
+    };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: '1.0',
+        sessionAttributes,
+        response: speechletResponse,
+    };
+}
+
+
+function getWelcomeResponse(callback) {
+
+    const sessionAttributes = {};
+    const cardTitle = 'Welcome';
+    const speechOutput = 'Welcome to the Alexa Skills Kit sample. ' +
+        'Please tell me your favorite color by saying, my favorite color is red';
+
+    const repromptText = 'Please tell me your favorite color by saying, ' +
+        'my favorite color is red';
+    const shouldEndSession = false;
+
+    callback(sessionAttributes,
+        buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
+
+function handleSessionEndRequest(callback) {
+    const cardTitle = 'Session Ended';
+    const speechOutput = 'Thank you for trying the Alexa Skills Kit sample. Have a nice day!';
+
+    const shouldEndSession = true;
+
+    callback({}, buildSpeechletResponse(cardTitle, speechOutput, null, shouldEndSession));
+}
+
+function createFavoriteColorAttributes(favoriteColor) {
+    return {
+        favoriteColor,
+    };
+}
+
+
+function setColorInSession(intent, session, callback) {
+    const cardTitle = intent.name;
+    const favoriteColorSlot = intent.slots.Color;
+    let repromptText = '';
+    let sessionAttributes = {};
+    const shouldEndSession = false;
+    let speechOutput = '';
+
+    if (favoriteColorSlot) {
+        const favoriteColor = favoriteColorSlot.value;
+        sessionAttributes = createFavoriteColorAttributes(favoriteColor);
+        speechOutput = `I now know your favorite color is ${favoriteColor}. You can ask me ` +
+            "your favorite color by saying, what's my favorite color?";
+        repromptText = "You can ask me your favorite color by saying, what's my favorite color?";
+    } else {
+        speechOutput = "I'm not sure what your favorite color is. Please try again.";
+        repromptText = "I'm not sure what your favorite color is. You can tell me your " +
+            'favorite color by saying, my favorite color is red';
+    }
+
+    callback(sessionAttributes,
+        buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
+
+function getColorFromSession(intent, session, callback) {
+    let favoriteColor;
+    const repromptText = null;
+    const sessionAttributes = {};
+    let shouldEndSession = false;
+    let speechOutput = '';
+
+    if (session.attributes) {
+        favoriteColor = session.attributes.favoriteColor;
+    }
+
+    if (favoriteColor) {
+        speechOutput = `Your favorite color is ${favoriteColor}. Goodbye.`;
+        shouldEndSession = true;
+    } else {
+        speechOutput = "I'm not sure what your favorite color is, you can say, my favorite color " +
+            ' is red';
+    }
+
+
+    callback(sessionAttributes,
+        buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
+}
+
+function WhoOwesMe(intent, sesison, callback){
+    let favoriteColor;
+    const repromptText = null;
+    const sessionAttributes = {};
+    let shouldEndSession = false;
+    let speechOutput = '';
+
+    if (session.attributes) {
+        favoriteColor = session.attributes.favoriteColor;
+    }
+
+    if (favoriteColor) {
+        speechOutput = `Your favorite color is ${favoriteColor}. Goodbye.`;
+        shouldEndSession = true;
+    } else {
+        speechOutput = "I'm not sure what your favorite color is, you can say, my favorite color " +
+            ' is red';
+    }
+
+
+    callback(sessionAttributes,
+        buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
+}
+
+function RequestMoney(intent, session, callback){
+    let favoriteColor;
+    const repromptText = null;
+    const sessionAttributes = {};
+    let shouldEndSession = false;
+    let speechOutput = '';
+
+    if (session.attributes) {
+        favoriteColor = session.attributes.favoriteColor;
+    }
+
+    if (favoriteColor) {
+        speechOutput = `Your favorite color is ${favoriteColor}. Goodbye.`;
+        shouldEndSession = true;
+    } else {
+        speechOutput = "I'm not sure what your favorite color is, you can say, my favorite color " +
+            ' is red';
+    }
+
+
+    callback(sessionAttributes,
+        buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
+}
+
+function SendMoney(intent, session, callback0){
+    let favoriteColor;
+    const repromptText = null;
+    const sessionAttributes = {};
+    let shouldEndSession = false;
+    let speechOutput = '';
+
+    if (session.attributes) {
+        favoriteColor = session.attributes.favoriteColor;
+    }
+
+    if (favoriteColor) {
+        speechOutput = `Your favorite color is ${favoriteColor}. Goodbye.`;
+        shouldEndSession = true;
+    } else {
+        speechOutput = "I'm not sure what your favorite color is, you can say, my favorite color " +
+            ' is red';
+    }
+
+
+    callback(sessionAttributes,
+        buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
+}
+
+function ShowPaymentHistory(intent, session, callback){
+    let favoriteColor;
+    const repromptText = null;
+    const sessionAttributes = {};
+    let shouldEndSession = false;
+    let speechOutput = '';
+
+    if (session.attributes) {
+        favoriteColor = session.attributes.favoriteColor;
+    }
+
+    if (favoriteColor) {
+        speechOutput = `Your favorite color is ${favoriteColor}. Goodbye.`;
+        shouldEndSession = true;
+    } else {
+        speechOutput = "I'm not sure what your favorite color is, you can say, my favorite color " +
+            ' is red';
+    }
+
+
+    callback(sessionAttributes,
+        buildSpeechletResponse(intent.name, speechOutput, repromptText, shouldEndSession));
+}
+
+
+function onSessionStarted(sessionStartedRequest, session) {
+    console.log(`onSessionStarted requestId=${sessionStartedRequest.requestId}, sessionId=${session.sessionId}`);
+}
+
+
+function onLaunch(launchRequest, session, callback) {
+    console.log(`onLaunch requestId=${launchRequest.requestId}, sessionId=${session.sessionId}`);
+
+
+    getWelcomeResponse(callback);
+}
+
+
+function onIntent(intentRequest, session, callback) {
+    console.log(`onIntent requestId=${intentRequest.requestId}, sessionId=${session.sessionId}`);
+
+    const intent = intentRequest.intent;
+    const intentName = intentRequest.intent.name;
+
+    // Dispatch to your skill's intent handlers
+    if (intentName === 'MyColorIsIntent') {
+        setColorInSession(intent, session, callback);
+    } else if (intentName === 'WhatsMyColorIntent') {
+        getColorFromSession(intent, session, callback);
+    }else if (intentName === 'WhoOwesMe') {
+        WhoOwesMe(intent, session, callback);
+    }else if (intentName === 'RequestMoney') {
+        RequestMoney(intent, session, callback);
+    }else if(intentName === 'SendMoney'){
+        SendMoney(intent, session, callback);
+    }else if(intentName === 'ShowPaymentHistory'){
+        ShowPaymentHistory(intent, session, callback);
+    } else if (intentName === 'AMAZON.HelpIntent') {
+        getWelcomeResponse(callback);
+    } else if (intentName === 'AMAZON.StopIntent' || intentName === 'AMAZON.CancelIntent') {
+        handleSessionEndRequest(callback);
+    } else {
+        throw new Error('Invalid intent');
+    }
+}
+
+
+function onSessionEnded(sessionEndedRequest, session) {
+    console.log(`onSessionEnded requestId=${sessionEndedRequest.requestId}, sessionId=${session.sessionId}`);
+
+}
+
+
+
+exports.handler = (event, context, callback) => {
+    try {
+        console.log(`event.session.application.applicationId=${event.session.application.applicationId}`);
+
+
+        if (event.session.new) {
+            onSessionStarted({ requestId: event.request.requestId }, event.session);
+        }
+
+        if (event.request.type === 'LaunchRequest') {
+            onLaunch(event.request,
+                event.session,
+                (sessionAttributes, speechletResponse) => {
+                    callback(null, buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === 'IntentRequest') {
+            onIntent(event.request,
+                event.session,
+                (sessionAttributes, speechletResponse) => {
+                    callback(null, buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === 'SessionEndedRequest') {
+            onSessionEnded(event.request, event.session);
+            callback();
+        }
+    } catch (err) {
+        callback(err);
+    }
+};
